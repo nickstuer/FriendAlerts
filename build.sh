928c57517ff3491c81b75abce2d85550f353865c
Nick Stuer <nickstuer@gmail.com>
2025-04-23 13:14:39 -0400

    minor change to build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ BUILD_DIRECTORY=$"${PWD}/dist"
 
 rm -vrf "$BUILD_DIRECTORY" | echo "$(wc -l) files deleted"
 mkdir -p "$BUILD_DIRECTORY"
+mkdir -p $"${BUILD_DIRECTORY}/temp"
 
-cp -R "src\Media" "${BUILD_DIRECTORY}\Media";
-cp -R "src\Options" "${BUILD_DIRECTORY}\Options";
-cp -R "src\Main.lua" "${BUILD_DIRECTORY}\Main.lua";
-cp -R "src\FriendAlerts.toc" "${BUILD_DIRECTORY}\FriendAlerts.toc";
-cp -R "README.md" "${BUILD_DIRECTORY}\README.md";
+cp -R "src\Media" "${BUILD_DIRECTORY}/temp/Media";
+cp -R "src\Options" "${BUILD_DIRECTORY}/temp/Options";
+cp -R "src\Main.lua" "${BUILD_DIRECTORY}/temp/Main.lua";
+cp -R "src\FriendAlerts.toc" "${BUILD_DIRECTORY}/temp/FriendAlerts.toc";
+cp -R "README.md" "${BUILD_DIRECTORY}/temp/README.md";
 
-(cd dist && "C:\Program Files\Git\bin\zip.exe" -r $"../${FILENAME}" .)
+(cd dist/temp && "C:\Program Files\Git\bin\zip.exe" -r $"../${FILENAME}" .)
