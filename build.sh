31e91500162a036816308e6013313e448b17af75
Nick Stuer <nickstuer@gmail.com>
2025-04-24 16:58:12 -0400

    Fix build to include addon name folder

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ BUILD_DIRECTORY=$"${PWD}/dist"
 rm -vrf "$BUILD_DIRECTORY" | echo "$(wc -l) files deleted"
 mkdir -p "$BUILD_DIRECTORY"
 mkdir -p $"${BUILD_DIRECTORY}/temp"
+mkdir -p $"${BUILD_DIRECTORY}/temp/FriendAlerts"
 
-cp -R "src/Media" "${BUILD_DIRECTORY}/temp/Media";
-cp -R "src/Settings" "${BUILD_DIRECTORY}/temp/Options";
-cp -R "src/Main.lua" "${BUILD_DIRECTORY}/temp/Main.lua";
-cp -R "src/FriendAlerts.toc" "${BUILD_DIRECTORY}/temp/FriendAlerts.toc";
-cp -R "README.md" "${BUILD_DIRECTORY}/temp/README.md";
+cp -R "src/Media" "${BUILD_DIRECTORY}/temp/FriendAlerts/Media";
+cp -R "src/Settings" "${BUILD_DIRECTORY}/temp/FriendAlerts/Options";
+cp -R "src/Main.lua" "${BUILD_DIRECTORY}/temp/FriendAlerts/Main.lua";
+cp -R "src/FriendAlerts.toc" "${BUILD_DIRECTORY}/temp/FriendAlerts/FriendAlerts.toc";
+cp -R "README.md" "${BUILD_DIRECTORY}/temp/FriendAlerts/README.md";
 
 if [ "$OSTYPE" == "msys" ]; then
     (cd dist/temp && "C:\Program Files\Git\bin\zip.exe" -r $"../${FILENAME}" .)
